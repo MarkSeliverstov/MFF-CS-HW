edc34f3f2aac415b738cccf78b2200ab969f62af
Mark <smdmrr00@gmail.com>
2022-11-11 00:18:25 +0100

    Create Huffman tree

--- ProgrammingInCS/Huffman/Program.cs
+++ ProgrammingInCS/Huffman/Program.cs
@@ -1,2 +1,146 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+
+namespace Huffman
+{
+    class Node{
+        public int value;
+        public decimal? ch;
+        public decimal order;
+        public Node? Left, Right;
+    }
+
+    class BinaryTree{
+        public Node root;
+
+        public BinaryTree(int value, decimal? ch, decimal order, Node Left, Node Right){
+            this.root.Left = Left;
+            this.root.Right = Right;
+            this.root.value = value;
+            this.root.ch = ch;
+            this.root.order = order;
+        }
+    }
+
+    class Reader{
+        private TextReader r;
+
+        public Reader(TextReader reader){
+            this.r = reader;
+        }
+
+        public int? ReadChar(){
+            return r.Read();
+        }
+
+    }
+
+    class Writer{   
+        private TextWriter w;
+
+        public Writer(TextWriter writer){
+            this.w = writer;
+        }
+
+        public static void WriteTree(BinaryTree tree){
+            
+        }
+    }
+
+    class Program{
+        public static void Main(string[] args){ 
+            // if(args.Length != 1){
+            //     Console.WriteLine("Argument Error");
+            //     return;
+            // }
+            // string fName = args[0];
+
+            string fName = "huffman-data/simpl.in";
+            try{
+                TextReader r = new StreamReader(fName);
+                TextWriter w = new StreamWriter("result.out");
+                Reader reader = new Reader(r);
+                Writer writer = new Writer(w);
+
+                List<BinaryTree> forest = CreateForest(r);
+                if (forest == null) return;
+                BinaryTree tree = CreateHuffmanTree(forest);
+                Writer.WriteTree(tree);
+            }
+            catch(FileNotFoundException){
+                Console.WriteLine("File Error");
+            }
+        }   
+
+        public static List<BinaryTree> CreateForest(TextReader r){
+            List<BinaryTree> forest = new();
+            Dictionary<int, int> WeightCharsInText = new();
+            int ch;
+
+            // Create Dict
+            while ((ch = r.Read()) != null)
+            {
+                WeightCharsInText[ch] = WeightCharsInText.ContainsKey(ch) ? 
+                    WeightCharsInText[ch]++ : WeightCharsInText[ch] = 1;
+            }
+
+            if (forest.Count() == 0) return null;
+
+            // Create forest
+            foreach (var item in WeightCharsInText){
+                BinaryTree SinglNodeTree = new BinaryTree(item.Value, item.Key, 0, null, null);
+                forest.Add(SinglNodeTree);
+            }
+
+            // Sorted forest
+            forest = forest.OrderBy(v => v.root.value).ToList();
+
+            return forest;
+        }
+
+        public static BinaryTree CreateHuffmanTree(List<BinaryTree> forest){
+            while (forest.Count() != 1){
+                BinaryTree firstMin = forest.MinBy(x => x.root.value);
+                int firstValue = firstMin.root.value;
+                forest.Remove(firstMin);
+
+                BinaryTree secondMin = forest.MinBy(x => x.root.value);
+                int secondValue = secondMin.root.value;
+                forest.Remove(secondMin);
+
+                decimal order = firstMin.root.order < secondMin.root.order ?
+                    secondMin.root.order++ : firstMin.root.order++;
+                
+                int newValue = firstValue + secondValue;
+                BinaryTree newTree;
+                if (firstValue == secondValue){
+                    if (firstMin.root.ch == null && secondMin.root.ch == null){
+                        newTree = firstMin.root.ch < secondMin.root.ch ? 
+                            new BinaryTree(newValue, null, order, firstMin.root, secondMin.root) :
+                            new BinaryTree(newValue, null, order, secondMin.root, firstMin.root);
+                    }
+                    else if (firstMin.root.ch == null){
+                        new BinaryTree(newValue, null, order, firstMin.root, secondMin.root);
+                    }
+                    else if (secondMin.root.ch == null){
+                        new BinaryTree(newValue, null, order, secondMin.root, firstMin.root);
+                    }
+                    else{
+                        newTree = firstMin.root.order < secondMin.root.order ? 
+                            new BinaryTree(newValue, null, order, firstMin.root, secondMin.root) :
+                            new BinaryTree(newValue, null, order, secondMin.root, firstMin.root);
+                    }
+
+                }
+                else{
+                    newTree = firstValue < secondValue ? 
+                        new BinaryTree(newValue, null, order, firstMin.root, secondMin.root) :
+                        new BinaryTree(newValue, null, order, secondMin.root, firstMin.root);
+
+                }
+
+            }
+            return forest[0];
+        }
+    }
+}
---
+++ ProgrammingInCS/Huffman/README.md
@@ -0,0 +1,34 @@
+# Huffman tree
+- *The goal is to write a program capable of constructing and printing a Huffman tree for a specific file.*
+
+A *Huffman tree* is a binary tree whose leaf nodes contain all characters present in the input file at least once. The weight of a character refers to the number of occurrences of this character in the input file. For example, the data "xxyz" contain characters "x", "y" and "z", with "x" having the weight of 2 and both "y" and "z" having the weight of 1. Inner nodes of the tree do not contain any characters and their weight is defined as the sum of the weights of both their child nodes (inner nodes always have two child nodes).
+
+---
+
+
+
+**input:** name of the file in the form of a single command-line argument;
+
+**output:** printed out to standard output in the prefix notation (Leaf nodes should be printed in the format *SYMBOL:WEIGHT);
+
+**conditions:** 
+
+A following algorithm is used when building the tree.
+First, prepare all leaf nodes based on the frequency analysis of the input data and organize them in a forest of single-node trees. As long as the forest contains more than one tree, always remove two nodes with the lowest weight and add them as children of a newly-created node (the weight of which is the sum of the weights of the two nodes), then add this node to the forest. When adding the child nodes, keep in mind that the node with lower weight (or more generally, the lighter node, the node with higher priority) is always the left child. If two nodes have the same weight, following rules are applied (in this order) to determine which node has priority (these rules also determine which node should be on the left and which on the right for nodes with the same weight):
+
+- leaf nodes are lighter than inner nodes
+- among leaf nodes, those with lower-valued character have priority
+- among inner nodes, those that were created sooner by the algorithm have priority
+
+**Example:**
+```
+$>program.exe simple.in
+
+
+**simple.in**
+    aaabbc
+
+
+_standard output_
+    6 *97:3 3 *99:1 *98:2
+```
