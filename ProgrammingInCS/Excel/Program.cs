2878d6f720a1608ba3cc65bb21c501abb4102a65
Mark <smdmrr00@gmail.com>
2022-11-27 18:28:04 +0100

    init new project

---
+++ ProgrammingInCS/Excel/Program.cs
@@ -0,0 +1,35 @@
+﻿using System.Runtime.InteropServices;
+
+namespace Excel
+{
+    class Program{
+        ClassInterfaceAttribute Sheet{
+
+        }
+
+        Class Cell{
+
+        }
+
+        Class Formula{
+            
+        }
+
+        public static void Main(string[] args)
+        {
+            if (args.Length != 1){
+                Console.Write("Argument Error");
+                return;
+            }
+
+            FileStream fin = null;
+
+            try{
+
+            }
+            catch (FileNotFoundException){
+
+            }
+        }
+    }
+}
