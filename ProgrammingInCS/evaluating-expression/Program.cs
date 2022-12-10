387bf716a61e330872f7b7088f4b8153590494e0
Mark <smdmrr00@gmail.com>
2022-12-10 22:01:31 +0100

    more changes

--- ProgrammingInCS/evaluating-expression/Program.cs
+++ ProgrammingInCS/evaluating-expression/Program.cs
@@ -1,30 +1,82 @@
-﻿namespace EvalExpression
-{
-    class Tree{
+﻿using System;
 
+namespace EvaluatingExpression{
+    class Program{
+        static void Main(string[] args){
+            TextReader reader = Console.In;
+            TextWriter writer = Console.Out;
+
+            // Expression expression = new Expression(reader.ReadLine());
+            Expression expression = new Expression("- - 2000000000 4000000000");
+            expression.Recurcive_Evaluate(expression.root);
+            writer.WriteLine(expression.result);
+        }
     }
 
-    struct Node{
+    enum Operator{
+        Plus,
+        Minus,
+        Multiply,
+        Divide
+    }
+
+    enum Errors{
+        InvalidFormat,
+        DividedByZero,
+        OverflowError
+    }
+
+    class Node{
+        public bool isNegative = false;
         public int? value;
-        public char? op;
-        public Node(int? value, char? op){
+        public Operator? op;
+
+        public Node? parent;
+        public Node? left;
+        public Node? right;
+
+        public Node(int value){
             this.value = value;
+        }
+
+        public Node(Operator op){
             this.op = op;
         }
     }
 
-    class Program{
-        static void Main(string[] args){
-            // string expression = "+ ~ 1 3";
-            // string expression = "/ + - 5 2 * 2 + 3 3 ~ 2";
-            // string expression = "- - 2000000000 2100000000 2100000000";
-            // string expression = "+ 1 2 3";
-            // string expression = "- 2000000000 4000000000";
-            // string expression = "1+2*3";
-            string expression = "1+2*3";
-            int result = EvalExpression(expression);
-            Console.WriteLine(result);
+    class Expression{
+        public Node? root{get; private set;}
+        public Errors? error{get; private set;} // TODO: get to string
+        public int? result{get; private set;}
+
+        public Expression(string expression){
+            if (expression.Length == 0)
+                error = Errors.InvalidFormat;
+            else{
+                Node newRoot = TryLoadToTree(expression.Split(" ", StringSplitOptions.RemoveEmptyEntries));
+                if(error == null) this.root = newRoot; // if didn't get error
+            }
+            return;
         }
-        
+
+        private Node TryLoadToTree(string[] expression){
+            // TODO: create tree with while
+            Node root;
+
+            return null;
+        }
+
+        public void Recurcive_Evaluate(Node? root){
+            // TODO: evaluate tree fix
+            if (root.op != null)
+                Console.Write(root.op + " ");
+            else if (root.value != null){
+                if (root.isNegative) Console.Write("~ ");
+                Console.Write(root.value + " ");
+            }
+            if (root.left != null) Recurcive_Evaluate(root.left);
+            if (root.right != null) Recurcive_Evaluate(root.right);
+        }
     }
+
 }
