ec70567dc4c2d91429dd6c6f0909113527abbbf9
Mark <smdmrr00@gmail.com>
2022-12-12 23:44:05 +0100

    all fixing

--- ProgrammingInCS/evaluating-expression/Program.cs
+++ ProgrammingInCS/evaluating-expression/Program.cs
@@ -1,16 +1,33 @@
 ﻿using System;
-using Microsoft.VisualBasic;
+using System.Collections.Generic;
+using System.IO;
 
 namespace EvaluatingExpression{
+    #nullable enable
     class Program{
         static void Main(string[] args){
             TextReader reader = Console.In;
             TextWriter writer = Console.Out;
 
-            //Expression expression = new Expression(reader.ReadLine());
-            ExpressionTree expression = new ExpressionTree("~ ~ ~ 2 3");
-            expression.Recurcive_Evaluate(expression.root);
-            writer.WriteLine(expression.result);
+
+            try{
+                string? input = reader.ReadLine();
+                ExpressionTree expression = new ExpressionTree(input);
+                expression.Evaluate();
+                writer.WriteLine(expression.result);
+            }
+            catch (OverflowException){
+                writer.WriteLine("Overflow Error");
+                return;
+            }
+            catch (DivideByZeroException){
+                writer.WriteLine("Divide Error");
+                return;
+            }
+            catch (Exception e){
+                    writer.WriteLine(e.Message);
+                return;
+            }
         }
     }
 
@@ -23,17 +40,9 @@ namespace EvaluatingExpression{
         Undefined
     }
 
-    enum Errors{
-        InvalidFormat,
-        DividedByZero,
-        OverflowError
-    }
-
     abstract class Node{}
 
     sealed class NodeInt: Node{
-        public Node? left;
-        public Node? right;
         public int value;
         public NodeInt(int value) => this.value = value;
     }
@@ -53,19 +62,19 @@ namespace EvaluatingExpression{
 
     class ExpressionTree{
         public Node? root;
-        public Errors? error;
         public int? result;
 
-
-        public ExpressionTree(string expression){
+        public ExpressionTree(string? expression){
+            if (expression == null)
+                throw new Exception("Format Error");
             Queue<string> tokens = new Queue<string>(expression.Split(" ", StringSplitOptions.RemoveEmptyEntries));
-
             if (tokens.Count == 0)
-                this.error = Errors.InvalidFormat;
+                throw new Exception("Format Error");
             else{
                 Node? node = LoadToTree_rec(tokens);
-                if (tokens.Count > 0)
-                    this.error = Errors.InvalidFormat;
+
+                if (tokens.Count > 0 || node == null)
+                    throw new Exception("Format Error");
                 else
                     this.root = node;
             }
@@ -90,9 +99,8 @@ namespace EvaluatingExpression{
         }
         
         private Node? LoadToTree_rec(Queue<string> tokens){
-            if (!tokens.TryDequeue(out string token)){
-                this.error  = Errors.InvalidFormat;
-                return null;
+            if (!tokens.TryDequeue(out string? token)){
+                throw new Exception("Format Error");
             }
 
             if (uint.TryParse(token, out uint value)){
@@ -100,7 +108,6 @@ namespace EvaluatingExpression{
                     return new NodeInt(Convert.ToInt32(value));
                 }
                 else{
-                    this.error = Errors.InvalidFormat;
                     return null;
                 }
             }  
@@ -124,9 +131,69 @@ namespace EvaluatingExpression{
                         return node;
                 }
                 else{
-                    this.error = Errors.InvalidFormat;
                     return null;
                 }
+            }
+        }
+
+        public void Evaluate(){
+            this.result = Evaluate_rec(this.root);
+        }
+
+        private int Evaluate_rec(Node? node){
+            if (node == null)
+                throw new Exception("Format Error");
+            else if (node is NodeInt){
+                NodeInt nodeInt = node as NodeInt;
+                return nodeInt.value;
+            }
+            else if (node is NodeBinOp)
+                return Evaluate_BinOp(node as NodeBinOp);
+            else if (node is NodeUnarOp)
+                return Evaluate_UnarOp(node as NodeUnarOp);
+            else
+                throw new Exception("Format Error");
+        }
+
+        private int Evaluate_BinOp(NodeBinOp node){
+            const decimal maxInt = 2147483647;
+            long left = Convert.ToInt64(Evaluate_rec(node.left));
+            long right = Convert.ToInt64(Evaluate_rec(node.right));
+            long result;
+            
+            switch (node.op){
+                case Operator.Plus:
+                    result = left + right;
+                    if (result > maxInt || result < -maxInt)
+                        throw new OverflowException();
+                    break;
+                case Operator.Minus:
+                    result =  left - right;
+                    if (result > maxInt || result < -maxInt)
+                        throw new OverflowException();
+                    break;
+                case Operator.Multiply:
+                    result = left * right;
+                    if (result > maxInt || result < -maxInt)
+                        throw new OverflowException();
+                    break;
+                case Operator.Divide:
+                    result = left / right;
+                    break;
+                default:
+                    throw new Exception("Format Error");
+            }
+    
+            return Convert.ToInt32(result);
+        }
+
+        private int Evaluate_UnarOp(NodeUnarOp node){
+            int next = Evaluate_rec(node.next);
+            switch (node.op){
+                case Operator.NextIsNegativeInt:
+                    return -next;
+                default:
+                    throw new Exception("Format Error");
             }
         }
     }
