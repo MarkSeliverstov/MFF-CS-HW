0f430b820d540159bd334706bd8dff29fb0606e8
Mark <smdmrr00@gmail.com>
2022-12-30 18:27:43 +0100

    new version of NET

--- ProgrammingInCS/evaluating-expression/Program.cs
+++ ProgrammingInCS/evaluating-expression/Program.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using System.IO;
 
-namespace EvaluatingExpression{
+namespace evaluating_expression{
     #nullable enable
     class Program{
         static void Main(string[] args){
