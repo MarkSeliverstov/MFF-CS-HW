7f4d7a33429e5392a173e999f819828f2725a719
Mark <smdmrr00@gmail.com>
2022-12-30 18:16:48 +0100

    done

--- ProgrammingInCS/evaluating-expression-2/Program.cs
+++ ProgrammingInCS/evaluating-expression-2/Program.cs
@@ -1,2 +1,376 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Collections.Generic;
+
+namespace ExpressionEvaluator2;
+#nullable enable
+abstract class Expression
+{
+    public static Expression? ParsePrefixExpression(string exprString)
+    {
+        string[] tokens = exprString.Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+
+
+        Expression? result = null;
+        Stack<OperatorExpression> unresolved = new Stack<OperatorExpression>();
+
+
+
+        foreach (string token in tokens)
+        {
+            if (result != null)
+            {
+                // We correctly parsed the whole tree, but there was at least one more unprocessed token left.
+                // This implies incorrect input, thus return null.
+
+                return null;
+            }
+
+            switch (token)
+            {
+                case "+":
+                    unresolved.Push(new PlusExpression());
+                    break;
+
+                case "-":
+                    unresolved.Push(new MinusExpression());
+                    break;
+
+                case "*":
+                    unresolved.Push(new MultiplyExpression());
+                    break;
+
+                case "/":
+                    unresolved.Push(new DivideExpression());
+                    break;
+
+                case "~":
+                    unresolved.Push(new UnaryMinusExpression());
+                    break;
+
+                default:
+                    if (!int.TryParse(token, out var value))
+                    {
+                        return null;    // Invalid token format
+                    }
+
+                    Expression? expr = new ConstantExpression(value);
+                    while (unresolved.Count > 0)
+                    {
+                        OperatorExpression oper = unresolved.Peek();
+                        if (oper.AddOperand(expr))
+                        {
+                            unresolved.Pop();
+                            expr = oper;
+                        }
+                        else
+                        {
+                            expr = null;
+                            break;
+                        }
+                    }
+
+                    if (expr != null)
+                    {
+                        result = expr;
+                    }
+
+                    break;
+            }
+        }
+
+        return result;
+    }
+
+    public abstract int Evaluate();
+    public abstract double Evaluate_double();
+}
+
+abstract class ValueExpression : Expression
+{
+    public abstract int Value
+    {
+        get;
+    }
+
+    public sealed override int Evaluate()
+    {
+        return Value;
+    }
+
+    public sealed override double Evaluate_double()
+    {
+        return Convert.ToDouble(Value);
+    }
+}
+
+sealed class ConstantExpression : ValueExpression
+{
+    public ConstantExpression(int value)
+    {
+        Value = value;
+    }
+
+    public override int Value { get; }
+}
+
+abstract class OperatorExpression : Expression
+{
+    public abstract bool AddOperand(Expression op);
+}
+
+abstract class UnaryExpression : OperatorExpression
+{
+    protected Expression? operand;
+
+    public Expression Operand => operand!;
+
+    public override bool AddOperand(Expression op)
+    {
+        if (operand == null)
+        {
+            operand = op;
+        }
+        return true;
+    }
+
+    public sealed override int Evaluate()
+    {
+        return Compute(operand!.Evaluate());
+    }
+
+    public sealed override double Evaluate_double()
+    {
+        return Compute_double(operand!.Evaluate());
+    }
+
+    protected abstract int Compute(int opValue);
+    protected abstract double Compute_double(double opValue);
+}
+
+abstract class BinaryExpression : OperatorExpression
+{
+    protected Expression? leftOperand, rightOperand;
+
+    public Expression LeftOperand => leftOperand!;
+    public Expression RightOperand => rightOperand!;
+
+    public override bool AddOperand(Expression op)
+    {
+        if (leftOperand == null)
+        {
+            leftOperand = op;
+            return false;
+        }
+        if (rightOperand == null)
+        {
+            rightOperand = op;
+        }
+        return true;
+    }
+
+    public sealed override int Evaluate()
+    {
+        return Compute(leftOperand!.Evaluate(), rightOperand!.Evaluate());
+    }
+
+    public sealed override double Evaluate_double()
+    {
+        return Compute_double(leftOperand!.Evaluate_double(), rightOperand!.Evaluate_double());
+    }
+
+    protected abstract int Compute(int op0Value, int op1Value);
+    protected abstract double Compute_double(double op0Value, double op1Value);
+}
+
+sealed class PlusExpression : BinaryExpression
+{
+    protected override int Compute(int op0Value, int op1Value)
+    {
+        return checked(op0Value + op1Value);
+    }
+
+    protected override double Compute_double(double op0Value, double op1Value)
+    {
+        return op0Value + op1Value;
+    }
+
+}
+
+sealed class MinusExpression : BinaryExpression
+{
+    protected override int Compute(int op0Value, int op1Value)
+    {
+        return checked(op0Value - op1Value);
+    }
+
+    protected override double Compute_double(double op0Value, double op1Value)
+    {
+        return op0Value - op1Value;
+    }
+}
+
+sealed class MultiplyExpression : BinaryExpression
+{
+    protected override int Compute(int op0Value, int op1Value)
+    {
+        return checked(op0Value * op1Value);
+    }
+
+    protected override double Compute_double(double op0Value, double op1Value)
+    {
+        return op0Value * op1Value;
+    }
+}
+
+sealed class DivideExpression : BinaryExpression
+{
+    protected override int Compute(int op0Value, int op1Value)
+    {
+        return (op0Value / op1Value);
+    }
+
+    protected override double Compute_double(double op0Value, double op1Value)
+    {
+        return (op0Value / op1Value);
+    }
+}
+
+sealed class UnaryMinusExpression : UnaryExpression
+{
+    protected override int Compute(int opValue)
+    {
+        return checked(-opValue);
+    }
+
+    protected override double Compute_double(double opValue)
+    {
+        return -opValue;
+    }
+}
+
+abstract class Command
+{
+    public abstract void Execute();
+
+    public static Command? ParseCommand(string input)
+    {
+        string[] tokens = input.Split(' ', StringSplitOptions.RemoveEmptyEntries);
+
+        switch (tokens[0])
+        {
+            case "=":
+                if (input.Length < 3)
+                {
+                    Memory.expr = null;
+                    return null;
+                }
+                return new Calculate_cmd(input.Substring(2));
+
+            case "i":
+                return new Calculate_in_int();
+
+            case "d":
+                return new Calculate_in_double();
+
+            default:
+                return null;
+        }
+    }
+}
+
+sealed class Calculate_cmd : Command
+{
+    public Calculate_cmd(string input)
+    {
+        Memory.expr = Expression.ParsePrefixExpression(input);
+    }
+    
+    public override void Execute()
+    {
+        if (Memory.expr == null)
+        {
+            Console.WriteLine("Format Error");
+        }
+        else
+        {
+            Console.Write("");
+        }
+    }
+}
+
+sealed class Calculate_in_int : Command
+{
+    public override void Execute()
+    {
+        if (Memory.expr != null)
+        {
+            try {
+                Console.WriteLine(Memory.expr.Evaluate());
+            }
+            catch (OverflowException) {
+                Console.WriteLine("Overflow Error");
+            }
+            catch (DivideByZeroException) {
+                Console.WriteLine("Divide Error");
+            }
+        }
+        else
+        {
+            Console.WriteLine("Expression Missing");
+        }
+    }
+}
+
+sealed class Calculate_in_double : Command
+{
+    public override void Execute()
+    {
+        if (Memory.expr != null)
+        {
+            try {
+                Console.WriteLine(Memory.expr.Evaluate_double().ToString("F5"));
+            }
+            catch (OverflowException) {
+                Console.WriteLine("Overflow Error");
+            }
+            catch (DivideByZeroException) {
+                Console.WriteLine("Divide Error");
+            }
+        }
+        else
+        {
+            Console.WriteLine("Expression Missing");
+        }
+    }
+}
+
+class Memory{
+    public static Expression? expr = null;
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        string? input = Console.ReadLine()!;
+        while ((input != null && input != "end"))
+        {
+            if (input == "")
+            {
+                input = Console.ReadLine()!;
+                continue;
+            }
+
+            Command? cmd = Command.ParseCommand(input)!;
+
+            if (cmd == null){
+                Console.WriteLine("Format Error");
+            }
+            else{
+                cmd.Execute();
+            }
+
+            input = Console.ReadLine()!;
+        }
+    }
+}
---
+++ ProgrammingInCS/evaluating-expression-2/README.md
@@ -0,0 +1,39 @@
+# Evaluating expression (calculator)
+
+## Solution with the [Visitor](https://en.wikipedia.org/wiki/Visitor_pattern) design pattern
+
+The goal is write a program capable of serving as a simple expression calculator. The program processes commands from standard input and prints results (or error messages) to standard output. There is only one command per line. Commands are processed sequentially; the previous command is fully executed before the next one starts loading. The calculator should terminate after it processes all input data (attempting to read a line results in `null`), or when it encounters a line containing only the string `"end"`. The calculator recognizes the following commands:
+
+- A line starting with the `"="` symbol followed by exactly one space and an expression in preorder format (see below). Such a line should be interpreted by parsing the expression and storing it; following operations will be done over the last parsed expression. If an expression was already stored, the previous expression should be discarded and replaced by the new expression. The previous expression will be discarded even if an error was encountered when processing the `"="` command.
+- A single string `"i"` should be interpreted by evaluating the last expression using integer arithmetic and printing out the result (one integer) on a single output line.
+- A single string `"d"` should be interpreted by evaluating the last expression using double-precision floating-point arithmetic (64 bits) and printing out the result on a single output line using 5 decimal places.
+The expression that follows the `"="` symbol may contain positive integers that should fit in a 32-bit signed integer type (i.e. are smaller than 231 = 2147483648), binary operators `+, -, * and /`, as well as the unary minus operator `~` representing additive inverse. Operators and numbers are separated with spaces.
+
+When evaluating using the integer arithmetic (`"i"` command), it is safe to assume that all subresults will fit in a 32-bit signed integer. If an overflow was encountered when evaluating the expression (i.e. subresult will not fit in a signed int), the program should print the following line as a result: `"Overflow Error"`. If division by zero was encountered, the following string should be printed: `"Divide Error"`.
+
+Evaluation of the expression using floating-point arithmetic is done in accordance with standard IEEE rules, i.e. including special values such as "infinity", "not a number", etc. If any such value is the result of an expression evaluation, it should be printed out in the standard text representation provided by the .NET libraries.
+
+If an invalid command is encountered, or if the textual representation of the expression is invalid for any reason (unknown tokens, not in preorder notation, etc.), the program should print the following line: `"Format Error"`. If the commands `"i"` or `"d"` are encountered and no valid expression is loaded, the program should print `"Expression Missing"` on a single line. If an empty line is encountered when processing the input, the program should ignore it without printing out any error.
+
+**Example (lines starting with "$>" represent input):**
+```
+**$>** i
+Expression Missing
+**$>** $#!%
+Format Error
+**$>** = + 2 3
+**$>** i
+5
+**$>** d
+5.00000
+**$>** = / 5 2
+**$>** i
+2
+**$>** d
+2.50000
+**$>** = +
+Format Error
+**$>** i
+Expression Missing
+**$>** end
+```
