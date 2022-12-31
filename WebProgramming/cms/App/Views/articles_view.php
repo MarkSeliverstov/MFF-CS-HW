b2b59013028a8e7da12f927f838b789b6ef0551e
Mark <smdmrr00@gmail.com>
2022-12-31 17:53:09 +0100

    new dir

--- WebProgramming/cms/App/Views/articles_view.php
+++ WebProgramming/cms/App/Views/articles_view.php
@@ -1,1 +1,24 @@
-<h1>Articles</h1>
+<div class="article__list">
+    <div>
+        <h1>Article list</h1>
+    </div>
+    <hr>
+    <div>
+        <?php foreach ($data as $article){ ?>
+        <div>
+            <p class="article__title__list"><?php echo $article['title']; ?></p>
+            <a class="show__btm" href="/article/<?php echo $article['id']; ?>">Show</a>
+            <a class="edit__btm" href="/article-edit/<?php echo $article['id']; ?>">Edit</a>
+            <a class="delete__btm" href="/article-edit/<?php echo $article['id']; ?>">Delete</a>
+        </div>
+        <?php } ?>
+    </div>
+    <hr>
+    <div>
+        <button class="previous__btm" >Previous</button>
+        <button class="next__btm">Next</button>
+        <p class="count__of__pages">page count: </p>
+        <button class="create__btm">Create article</button>
+    </div>
+</div>
+
--- WebProgramming/cms/App/models/model_articles.php
+++ WebProgramming/cms/App/models/model_articles.php
@@ -5,7 +5,13 @@ class Model_Articles extends Model
 	
 	public function get_data($id = null)
 	{
-        return null;
+        return array (
+            array(
+                'id' => 1,
+                'title' => 'Article title',
+                'content' => 'Article content',
+            ),
+        );
 	}
 
     public function get_article($id){
