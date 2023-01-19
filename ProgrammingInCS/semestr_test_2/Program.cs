22210ed8c3783c30ad29fd1de346b26187f4f8a0
Mark <smdmrr00@gmail.com>
2023-01-19 23:07:26 +0100

    create template for test

--- ProgrammingInCS/semestr_test_2/Program.cs
+++ ProgrammingInCS/semestr_test_2/Program.cs
@@ -1,2 +1,68 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+using System.Diagnostics.Contracts;
+
+namespace semestrProject;
+
+class Reader{
+    public Dictionary<string, StreamReader>? Many_sr;
+    public StreamReader? One_sr;
+
+    public Reader(string[] files){
+        if (files == null){ 
+            throw new NullReferenceException("files are null");
+        }
+        Many_sr = new Dictionary<string, StreamReader>();
+        foreach (string file in files){
+            try{
+                Many_sr.Add(file, new StreamReader(file));
+            } catch (FileNotFoundException){
+                Console.WriteLine($"File {file} not found");
+                return;
+            }
+        }
+    }
+
+    public Reader(StreamReader sr){
+        One_sr = sr;
+    }
+
+    public void CloseAllFiles(){
+    }
+}
+
+class Writer{
+    private StreamWriter? sw;
+
+    public Writer(string file){
+        try{
+            sw = new StreamWriter(file);
+        } catch (FileNotFoundException){
+            Console.WriteLine($"File {file} not found");
+            return;
+        }
+    }
+
+    public void Write(string str){
+        if (sw == null){
+            throw new NullReferenceException("file is null");
+        }
+        sw.Write(str);
+    }
+
+    public void CloseFile(){
+        if (sw == null){
+            throw new NullReferenceException("file is null");
+        }
+        sw.Close();
+    }
+}
+
+class Program{
+    static void Main(string[] args){
+        
+    }
+}
---
+++ ProgrammingInCS/semestr_test_2/Tests.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace semestr_test_2
+{
+    public class Tests
+    {
+        [Fact]
+        public void Test1()
+        {
+            Assert.True(true);
+        }
+    }
+}
