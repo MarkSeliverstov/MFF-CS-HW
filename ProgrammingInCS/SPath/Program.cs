50872e5db7057d59e380d35a2d908e623327ec03
Mark <smdmrr00@gmail.com>
2023-02-15 23:41:46 +0100

    Fix: Doesnt create new tree from first and second sub-tree

--- ProgrammingInCS/SPath/Program.cs
+++ ProgrammingInCS/SPath/Program.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.Common;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Text;
 
 namespace SPath
@@ -66,37 +68,147 @@ namespace SPath
         }
         
         string ReadQuery(StreamReader sr){
-            StringBuilder sb = new StringBuilder();
-            char ch = (char)sr.Read();
-            while (sr.EndOfStream == false)
-            {
-                if (ch != ' ' && ch != '\t'){
-                    ch = (char)sr.Read();
-                    sb.Append(ch);
-                }
-            }
-            return sb.ToString();
+            string line = sr.ReadLine()!;
+            string[] tokens = line.Split(' ', StringSplitOptions.RemoveEmptyEntries);
+            return string.Join("", tokens);
         }
 
-        public List<Node> Run(){
+        public void Run(){
+            string identifier = "";
+            string predicate = "";
             
+
+            foreach (char ch in this.query)
+            {
+                switch (ch)
+                {
+                    case '/':
+                        // this.set = this.set.SelectMany(x => x.childs).ToList();
+                        identifier = "";
+                        predicate = "";
+                        // Step(this.set);
+                        break;
+                    case '[': 
+
+                        break;
+                    case ']':
+
+                        break;
+                    default:
+                        // this.set = this.set.Where(x => x.data == ch.ToString()).ToList();
+                        break;
+                }
+            }
+        }
+
+        // private List<Node> Step(List<Node> set){
+            
+        // }
+
+        // private List<Node> Select(List<Node> set, string identifier){
+
+        // }
+
+        // private List<Node> Filter(List<Node> set, string predicate){
+
+        // }
+        
+    }
+
+    struct Step{
+        public string identifier {get;set;}
+        public List<Predicate>? predicates {get;set;}
+    }
+
+    struct Predicate{
+        public int? index {get;set;}   
+        public List<Step>? steps {get;set;}
+
+        public Predicate(StreamReader sr){    
+            char ch;
+            string identifier = "";
+            List<Predicate> predicates = new List<Predicate>();
+            List<Step> steps = new List<Step>();
+
+            while ((ch = (char)sr.Read()) != ']')
+            {
+                 switch (ch)
+                {
+                    case ' ': break;
+                    case '\t': break;
+                    case '\r': break;
+                    case '\n': break;
+                    case '/':
+                        steps.Add(new Step(){identifier = identifier, predicates = predicates});
+                        identifier = "";
+                        predicates = new List<Predicate>();
+                        break;
+                    case '[':
+                        predicates.Add(new Predicate(sr));
+                        break;
+                    case ']':
+                        if (int.TryParse(identifier, out int index))
+                            this.index = index;
+                        else
+                            this.steps = steps;
+                        break;
+                    default:
+                        identifier += ch;
+                        break;
+                }
+            }
         }
     }
 
+    // /a/b[a/b][a[a/b[v[1]]]][2]/c 
     class Program
     {
+
+        static List<Step> ReadQuery(StreamReader sr){
+            char ch;
+            string identifier = "";
+            List<Step> steps = new List<Step>();
+            List<Predicate> predicates = new List<Predicate>();
+            while (sr.EndOfStream == false)
+            {
+                ch = (char)sr.Read();
+                switch (ch)
+                {
+                    case ' ': break;
+                    case '\t': break;
+                    case '\r': break;
+                    case '\n': break;
+                    case '/':
+                        if (identifier != "")
+                            steps.Add(new Step(){identifier = identifier, predicates = predicates});
+                        identifier = "";
+                        predicates = new List<Predicate>();
+                        break;
+                    case '[':
+                        predicates.Add(new Predicate(sr));
+                        break;
+                    default:
+                        identifier += ch;
+                        break;
+                }
+            }
+            steps.Add(new Step(){identifier = identifier, predicates = predicates});
+            return steps;
+        }
+
         static void Main(string[] args)
         {
             StreamReader srData = new StreamReader("data.in");
             StreamReader srQuery = new StreamReader("query.in");
             StreamWriter sw = new StreamWriter("result.out");
 
-            Tree tree = new Tree();
-            tree.BuildTree(srData);
+            // Tree tree = new Tree();
+            // tree.BuildTree(srData);
 
-            Query query = new Query(srQuery, tree);
-            List<Node> result = query.Run();
+            // Query query = new Query(srQuery, tree);
+            // List<Node> result = query.Run();
 
+            List<Step> steps = ReadQuery(srQuery);
             srQuery.Close();
             srData.Close();
         }
