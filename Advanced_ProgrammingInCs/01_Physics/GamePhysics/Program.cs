a589f3d7bd0163e0caafa1eae5ae2c58bf318bbf
Mark <smdmrr00@gmail.com>
2023-03-03 17:06:53 +0100

    add types

--- Advanced_ProgrammingInCs/01_Physics/GamePhysics/Program.cs
+++ Advanced_ProgrammingInCs/01_Physics/GamePhysics/Program.cs
@@ -2,6 +2,41 @@
 
 namespace GamePhysics
 {
+    public struct Meter{
+        public double Value { get; set; }
+        public override string ToString() => $"{Value}m";
+        public Meter(double value) => Value = value;
+
+        public static Meter operator +(Meter a, Meter b) => new Meter(a.Value + b.Value);
+        public static Speed operator /(Meter distance, Second sec) => new Speed(distance.Value / sec.Value);
+    }
+
+    public struct Speed{
+        public double Value { get; set; }
+        public override string ToString() => $"{Value}m/s";
+        public Speed(double value) => Value = value;
+
+        public static Speed operator *(Speed speed, double factor) => new Speed(speed.Value * factor);
+    }
+
+    public struct Second{
+        public double Value { get; set; }
+        public override string ToString() => $"{Value}s";
+        public Second(double value) => Value = value;
+        //public static implicit operator int(Second sec) => (int)sec.Value;
+    }
+
+    public static class GamePhysicsExtensions
+    {
+        public static Meter Meters(this double value) => new Meter(value);
+        public static Meter Meters(this int value) => new Meter(value);
+        
+        public static Second Seconds(this double value) => new Second(value);
+        public static Second Seconds(this int value) => new Second(value);
+
+        public static Speed MeterPerSeconds(this double value) => new Speed(value);
+    }
+
     class Program
     {
         static void Main(string[] args)
@@ -21,6 +56,7 @@ namespace GamePhysics
             speed = 3.5.MeterPerSeconds();
             Console.WriteLine($"New speed: {speed}");
 
+            // int valueSec = (int)time;
             // !!! Uncommenting following line must produce Error: Operator '*=' cannot be applied to operands of type 'MeterPerSecond' and 'Meter'
             // speed *= distance;
             // !!! Uncommenting following line must produce Error: Operator '+=' cannot be applied to operands of type 'Meter' and 'Second'
