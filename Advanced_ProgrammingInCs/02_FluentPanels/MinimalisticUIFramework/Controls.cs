853246bae8eeb9a7fd62149c5771e897cac1a5e3
Mark <smdmrr00@gmail.com>
2023-03-09 15:09:51 +0100

    new project

---
+++ Advanced_ProgrammingInCs/02_FluentPanels/MinimalisticUIFramework/Controls.cs
@@ -0,0 +1,22 @@
+﻿namespace MinimalisticUIFramework {
+	public abstract class Control {
+	}
+
+	public sealed class Image : Control {
+		public string Url { get; private set; } = "";
+		public float ZoomFactor { get; private set; } = 1.0f;
+
+		public Image WithUrl(string url) { Url = url; return this; }
+		public Image WithZoomFactor(float zoomFactor) { ZoomFactor = zoomFactor; return this; }
+
+		public override string ToString() => $"Image {{ Url = \"{Url}\", ZoomFactor = {ZoomFactor} }}";
+	}
+
+	public sealed class Label : Control {
+		public string Text { get; private set; } = "";
+
+		public Label WithText(string text) { Text = text; return this; }
+
+		public override string ToString() => $"Label {{ Text = \"{Text}\" }}";
+	}
+}
