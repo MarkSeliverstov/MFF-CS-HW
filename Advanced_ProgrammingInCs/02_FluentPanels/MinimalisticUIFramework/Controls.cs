aa3fb5a1e6fbe594ac60e35603b275484c599614
Mark <smdmrr00@gmail.com>
2023-03-12 22:45:41 +0100

    part 2 - done

--- Advanced_ProgrammingInCs/02_FluentPanels/MinimalisticUIFramework/Controls.cs
+++ Advanced_ProgrammingInCs/02_FluentPanels/MinimalisticUIFramework/Controls.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.VisualBasic;
+using System.Reflection.Emit;
 using System.Text;
 
 namespace MinimalisticUIFramework {
@@ -22,6 +23,13 @@ namespace MinimalisticUIFramework {
 		public override string ToString() => $"Label {{ Text = \"{Text}\" }}";
 	}
 
+	public class Point{
+		public int X { get; set; }
+		public int Y { get; set; }
+
+		public override string ToString() => $"{X}, {Y}";
+	}
+
 	public sealed class StackPanel : Control {
 		private List<Control> childs = new List<Control>();
 		public void AddChild(Control child) => childs.Add(child);
@@ -40,6 +48,7 @@ namespace MinimalisticUIFramework {
 	public sealed class Canvas{
 		private List<Control> childs = new List<Control>();
 		private List<Point> positions = new List<Point>();
+		
 		public void AddChild(Control child, Point position) {
 			childs.Add(child);
 			positions.Add(position);
@@ -51,15 +60,56 @@ namespace MinimalisticUIFramework {
 			for (int i = 0; i < childs.Count; i++) {
 				sb.AppendLine($"{childs[i]} at {positions[i]}");
 			}
-			sb.AppendLine("}");
+			sb.Append("}");
 			return sb.ToString();
 		}
 	}
 
-	public class Point{
-		public int X { get; set; }
-		public int Y { get; set; }
+	public static class Placer{
+		// For StackPanel
+		public static T PlacedIn<T>(this T control, StackPanel stackPanel) where T : Control {
+			switch (control) {
+				case Image image:
+					stackPanel.AddChild(image);
+					return control;
+				case Label label:
+					stackPanel.AddChild(label);
+					return control;
+				case StackPanel stackPanel2:
+					stackPanel.AddChild(stackPanel2);
+					return control;
+				default:
+					throw new Exception("This type of control is not supported");
+			}
+		}
 
-		public override string ToString() => $"({X}, {Y})";
+		// For Canvas
+		public static Tuple<Canvas, T> PlacedIn<T>(this T control, Canvas canvas) where T : Control {
+			switch (control) {
+				case Image image: return new Tuple<Canvas, T>(canvas, control);
+				case Label label: return new Tuple<Canvas, T>(canvas, control);
+				case StackPanel stackPanel: return new Tuple<Canvas, T>(canvas, control);
+				default: throw new Exception("This type of control is not supported");
+			}
+		}
+
+		public static T At<T>(this Tuple<Canvas, T> tuple, int x, int y) where T : Control {
+			var position = new Point { X = x, Y = y };
+
+			switch (tuple.Item2) {
+				case Image image:
+					tuple.Item1.AddChild(image, position);
+					return tuple.Item2;
+				case Label label:
+					tuple.Item1.AddChild(label, position);
+					return tuple.Item2;
+				case StackPanel stackPanel:
+					tuple.Item1.AddChild(stackPanel, position);
+					return tuple.Item2;
+				default:
+					throw new Exception("This type of control is not supported");
+			}
+		}
+
 	}
 }
