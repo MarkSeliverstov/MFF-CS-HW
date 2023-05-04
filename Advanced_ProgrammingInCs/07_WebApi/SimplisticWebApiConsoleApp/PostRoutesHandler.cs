38e91b2220dff124dc7b6395ce89b0f15addf107
Mark <smdmrr00@gmail.com>
2023-05-04 10:47:11 +0200

    2 task - done

--- Advanced_ProgrammingInCs/07_WebApi/SimplisticWebApiConsoleApp/PostRoutesHandler.cs
+++ Advanced_ProgrammingInCs/07_WebApi/SimplisticWebApiConsoleApp/PostRoutesHandler.cs
@@ -1,14 +1,20 @@
-﻿public class PostRoutesHandler : ISimplisticRoutesHandler {
-    public void RegisterRoutes(RouteMap routeMap) {
+﻿public class PostRoutesHandler : ISimplisticRoutesHandler
+{
+    public void RegisterRoutes(RouteMap routeMap)
+    {
         Console.WriteLine("+++ PostRoutesHandler.RegisterRoutes() called.");
 
         // TODO: Has to compile without any changes !!!
-        // routeMap.Map("/services/PostCode/getDataAsJson", GetPostCodesByCityAndStreet);
+        routeMap.Map("/services/PostCode/getDataAsJson", GetPostCodesByCityAndStreet);
+        // path = /services/PostCode/getDataAsJson?city=Praha&street=Patkova
     }
 
-    public IReadOnlyList<PostCodeItem> GetPostCodesByCityAndStreet(string cityOrPart, string nameStreet) {
-        return cityOrPart switch {
-            "Praha" => nameStreet switch {
+    public IReadOnlyList<PostCodeItem> GetPostCodesByCityAndStreet(string cityOrPart, string nameStreet)
+    {
+        return cityOrPart switch
+        {
+            "Praha" => nameStreet switch
+            {
                 "Patkova" => new[] {
                         new PostCodeItem("Praha", "Pátkova", "Praha 82", "18200", "2136/3"),
                     },
@@ -18,7 +24,8 @@
                     },
                 _ => Array.Empty<PostCodeItem>()
             },
-            "Sokolov" => nameStreet switch {
+            "Sokolov" => nameStreet switch
+            {
                 "Karla Hynka Machy" => new[] {
                     new PostCodeItem("Sokolov", "Karla Hynka Máchy", "Sokolov 1", "35601", "1100"),
                     new PostCodeItem("Sokolov", "Karla Hynka Máchy", "Sokolov 1", "35601", "2230"),
--- Advanced_ProgrammingInCs/07_WebApi/SimplisticWebApiConsoleApp/Program.cs
+++ Advanced_ProgrammingInCs/07_WebApi/SimplisticWebApiConsoleApp/Program.cs
@@ -1,7 +1,13 @@
-﻿class Program {
+﻿using System.Reflection;
+using System.Text.Json;
+using Microsoft.AspNetCore.WebUtilities;
+
+class Program
+{
     static RequestProcessor requestProcessor = new RequestProcessor();
 
-    static void Main(string[] args) {
+    static void Main(string[] args)
+    {
         var builder = WebApplication.CreateBuilder(args);
         var app = builder.Build();
 
@@ -13,7 +19,8 @@
         app.Run();
     }
 
-    static Task ProcessRequest(HttpContext context) {
+    static Task ProcessRequest(HttpContext context)
+    {
         var path = context.Request.Path.Value ?? "";
         var query = context.Request.QueryString.Value ?? "";
         var result = requestProcessor.HandleRequest(path, query);
@@ -21,28 +28,90 @@
     }
 }
 
-class RequestProcessor {
+class RequestProcessor
+{
     private RouteMap _routes = new RouteMap();
 
     public void RegisterAllRoutes()
     {
+        Assembly asm = Assembly.GetExecutingAssembly();
+        var test = asm.GetTypes().Where(t => t.GetInterfaces().Contains(typeof(ISimplisticRoutesHandler)));
+
+        var handlers = asm.GetTypes()
+            .Where(t => t.GetInterfaces().Contains(typeof(ISimplisticRoutesHandler)))
+            .Select(t => Activator.CreateInstance(t) as ISimplisticRoutesHandler);
+
+        foreach (var handler in handlers)
+        {
+            handler.RegisterRoutes(_routes);
+        }
     }
 
-    public string HandleRequest(string path, string query) {
+    public string HandleRequest(string path, string query)
+    {
         Console.WriteLine($"+++ Thread #{Thread.CurrentThread.ManagedThreadId} processing request:");
         Console.WriteLine($"    Path =\"{path}\"");
         Console.WriteLine($"    Query=\"{query}\" ...");
- 
-        Console.WriteLine($"!!! Parameter ABCDE is missing !!!");
-        Console.WriteLine($"!!! Route not found !!!");
-        
-        return "{}";
+        if (path == "/")
+        {
+            return "Home page";
+        }
+
+        if (_routes.ContainRoute(path))
+        {
+            var handler = _routes.GetHandler(path);
+            var queryDictionary = QueryHelpers.ParseQuery(query);
+            if (queryDictionary.ContainsKey("city"))
+            {
+                var city = queryDictionary["city"];
+                if (queryDictionary.ContainsKey("street"))
+                {
+                    var street = queryDictionary["street"];
+                    var result = handler(city, street);
+                    return JsonSerializer.Serialize(result);
+                }
+                else
+                {
+                    Console.WriteLine($"!!! parametr street not found !!!");
+                    return "Parametr street not found";
+                }
+            }
+            else
+            {
+                Console.WriteLine($"!!! parametr city not found !!!");
+                return "Parametr city not found";
+            }
+        }
+        else
+        {
+            Console.WriteLine($"!!! Route not found !!!");
+            return "Route not found";
+        }
     }
 }
 
-public class RouteMap {
+public class RouteMap
+{
+    private Dictionary<string, Func<string, string, IReadOnlyList<PostCodeItem>>> _routes = new Dictionary<string, Func<string, string, IReadOnlyList<PostCodeItem>>>();
+
+    public void Map(string path, Func<string, string, IReadOnlyList<PostCodeItem>> handler)
+    {
+        _routes.Add(path, handler);
+    }
+
+    public bool ContainRoute(string path)
+    {
+        return _routes.ContainsKey(path);
+    }
+
+    public Func<string, string, IReadOnlyList<PostCodeItem>> GetHandler(string path)
+    {
+        return _routes[path];
+    }
 }
 
-public interface ISimplisticRoutesHandler {
+public interface ISimplisticRoutesHandler
+{
     public void RegisterRoutes(RouteMap routeMap);
-} 
+}
+
