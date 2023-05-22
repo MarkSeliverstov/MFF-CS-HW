a12627317d64afa769045c16da6e42c991d512fa
Mark <smdmrr00@gmail.com>
2023-05-22 10:59:04 +0200

    Add new task and done

---
+++ Advanced_ProgrammingInCs/10_RobotSimulationCore/RobotSimulationLibCore/Robot.cs
@@ -0,0 +1,34 @@
+﻿//--------------------------------------------------------------------------
+// 
+//  Copyright (c) Microsoft Corporation.  All rights reserved. 
+// 
+//  File: Robot.cs
+//
+//--------------------------------------------------------------------------
+
+using System.Collections.Generic;
+
+namespace AntisocialRobots
+{
+    /// <summary>Represents one robot.</summary>
+    [System.Diagnostics.DebuggerDisplay("Location = {Location}")]
+    public class Robot
+    {
+
+        public Robot(bool isMovable = true)
+        {
+            IsMovable = isMovable;
+        }
+
+        public int Id;
+
+        /// <summary>The current location of the room.</summary>
+        public RoomPoint Location;
+
+        // frame of last move
+        public int lastmoved;
+
+        /// <summary>Indicates whether this robot is movable.</summary>
+        public bool IsMovable { get; private set; }
+    }
+}
